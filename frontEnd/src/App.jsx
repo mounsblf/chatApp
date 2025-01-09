7183f8ce393fe5b6ed955199c0eec2d67f1b243b
jade <jade@MacBook-Air-de-jade-2.local>
2025-01-09 02:34:30 +0100

    frontend routing + setup daisyUI Tailwind

---
+++ frontEnd/src/App.jsx
@@ -0,0 +1,23 @@
+import Navbar from './components/Navbar';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUpPage';
+import SettingsPage from './pages/SettingsPage';
+import ProfilePage from './pages/ProfilePage';
+
+const App = () => {
+  return <div>
+    const { authUser } = useAuthStore();
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignUpPage />} />
+      <Route path="/settings" element={<SettingsPage />} />
+      <Route path="/profile" element={<ProfilePage />} />
+    </Routes>
+  </div>
+};
+
+export default App;
